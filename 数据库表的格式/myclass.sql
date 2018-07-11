f9832fc9355b8a9e56ed31720c35e2bbb1470fc7
Gin.TaMa <1137554811@qq.com>
2018-07-11 08:37:11 +0800

    补充了一下score的表的格式

--- 数据库表的格式/myclass.sql
+++ 数据库表的格式/myclass.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 10/07/2018 19:11:37
+ Date: 10/07/2018 21:32:23
 */
 
 SET NAMES utf8mb4;
@@ -45,6 +45,26 @@ CREATE TABLE `class2016201114`  (
   `Cadr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `Cday` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`Cname`, `Ctime`, `Cday`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for score2016201110
+-- ----------------------------
+DROP TABLE IF EXISTS `score2016201110`;
+CREATE TABLE `score2016201110`  (
+  `课程名称` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `课程编号` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `开课学期` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `成绩` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `学分` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `总学时` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `考核方式` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `考试性质` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `课程属性` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `课程性质` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `通识教育选修课程类别` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `成绩标记` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`课程名称`, `课程编号`, `开课学期`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
